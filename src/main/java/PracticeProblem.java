d6a8e7ea50db994c8dd970839300eac8e666c875
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-04-16 15:42:30 +0000

    iyuv

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,4 +6,47 @@ public class PracticeProblem {
 
 	}
 
+	public static int intChecker(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input an integer: ");
+		while (!(input.hasNextInt())){
+			System.out.println("Input an integer: ");
+			input.nextLine();
+		}
+		return input.nextInt();
+	}
+
+	public static double doubleChecker(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input a number: ");
+		while (!(input.hasNextDouble())){
+			System.out.println("Input a number: ");
+			input.nextLine();
+		}
+		return input.nextDouble();
+	}
+
+	public static boolean booleanChecker(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input a boolean: ");
+		while (!(input.hasNextBoolean())){
+			System.out.println("Input a boolean: ");
+			input.nextLine();
+		}
+		return input.nextBoolean();
+	}
+
+
+	public static boolean multipleIntChecker(){
+		Scanner input = new Scanner(System.in);
+		for (int buhm = 0; buhm < 4 ; buhm++){
+		System.out.print("Input an integer: ");
+		while (!(input.hasNextInt())){
+			input.nextLine();
+			System.out.print("Input an integer: ");
+		}
+		input.nextLine();
+		}
+		return true;
+	}
 }
